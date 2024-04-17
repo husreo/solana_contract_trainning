b6f8f99c4a330323e1f7f4e3d61924dfaa783982
husreo <setodev1994@gmail.com>
2024-04-17 11:18:41 +0300

    base

--- staking-first/programs/staking-first/src/lib.rs
+++ staking-first/programs/staking-first/src/lib.rs
@@ -6,10 +6,28 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod staking_first {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, data:u64) -> Result<()> {
+        ctx.account.my_account.data = data;
         Ok(())
     }
 }
 
+#[accounts]
+pub struct MyAccount {
+    data: u64
+    mint: Pubkey
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize {
+    #[account(mut)]
+
+    pub my_account: Account<'info, MyAccont>
+
+    #[account(
+        constrain = my_account.mint == token_account.mint,
+        has_one = owner
+    )]
+    token_account: Account<'info, TokenAccount>
+    owner: Sign<'info>
+}
